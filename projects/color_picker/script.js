d4477527637eb9e0f0c1651e34bd6214453482de
Spandcynd <spandcynd@ukr.net>
2022-12-18 10:10:17 +0200

    Modified project

--- projects/color_picker/script.js
+++ projects/color_picker/script.js
@@ -1,5 +1,7 @@
 const columnsNode = document.querySelectorAll('.col')
+
 document.addEventListener('keydown', event => {
+  event.preventDefault()
   if (event.code === 'Space') {
     setRandomColors()
   }
@@ -13,11 +15,21 @@ document.addEventListener('click', event => {
     node.classList.toggle('fa-lock')
     node.classList.toggle('fa-lock-open')
   }
+
+  if (event.target.dataset.type === 'text-to-clipboard') {
+    const text = event.target.textContent
+    copyToClipBoard(text)
+  }
 })
 
 function setRandomColors() {
   const hexMap = '0123456789ABCDEF'
   columnsNode.forEach(col => {
+    columnText = col.querySelector('h2')
+    columnLock = col.querySelector('button')
+    if (col.querySelector('i').classList[1] === 'fa-lock') {
+      return
+    }
     hexColorArray = ['#']
     for (let i = 0; i < 6; i++) {
       randomHexSymbol = hexMap[Math.floor(Math.random() * 16)]
@@ -26,8 +38,6 @@ function setRandomColors() {
     hexColor = hexColorArray.join('')
 
     col.style.background = hexColor
-    columnText = col.querySelector('h2')
-    columnLock = col.querySelector('button')
     columnText.textContent = hexColor
     const luminance = chroma(hexColor).luminance()
     if (luminance > 0.5) {
@@ -38,6 +48,10 @@ function setRandomColors() {
       columnLock.style.color = 'white'
     }
   })
+}
+
+function copyToClipBoard(text) {
+  return navigator.clipboard.writeText(text)
 }
 
 setRandomColors()
